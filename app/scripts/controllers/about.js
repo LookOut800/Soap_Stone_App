7e96ad40837f2d81514c0d585a0c352e416a36ee
Charles Talbot <charlietalbot@gmail.com>
2015-04-06 09:56:56 -0400

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name soapStoneAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the soapStoneAppApp
+ */
+angular.module('soapStoneAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name soapStoneAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the soapStoneAppApp
+ */
+angular.module('soapStoneAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
